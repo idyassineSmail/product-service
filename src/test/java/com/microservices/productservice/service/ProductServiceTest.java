4a11823de360262d64f1c82dbf5a7b9fb7f4f3b2
idyassineSmail <ismailidyassin@gmail.com>
2023-12-16 15:58:02 +0100

    add docker secret

--- src/test/java/com/microservices/productservice/service/ProductServiceTest.java
+++ src/test/java/com/microservices/productservice/service/ProductServiceTest.java
@@ -7,10 +7,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,8 +25,15 @@ public class ProductServiceTest {
    @Test
    public void shouldGetAllProducts(){
        List<Product> productList=new ArrayList<>();
-   //    when(productRepository.findAll()).thenReturn(productList);
-       assertEquals(productList.size(),0);
+       productList.add(Product.builder()
+               .name("irisi")
+               .description("chi haja")
+               .price(BigDecimal.valueOf(14.2))
+               .build());
+        given(productRepository.findAll()).willReturn(productList);
+//       when(productRepository.findAll()).thenReturn(productList);
+       List<Product> products = productRepository.findAll();
+       assertEquals(productList.size(),1);
    }
 
 }
